cee090877dece457243a04fb4295aed687165d9f
eric hopper <eric.hopper@cozera.io>
2022-06-17 06:48:54 -0700

    updated keys

--- push.js
+++ push.js
@@ -4,20 +4,6 @@ var push = require("web-push");
 //console.log(keys);
 
 let keys = {
-  publicKey:
-    "BMDwYkYu4jJfJff4lAytigMUnEb0J7nv_LYLwFhV4I9o_-6Dvf_ceNLN8yshkpwlQovddTuz9tgOU9gUgOuFks0",
-  privateKey: "wsgBpk6li-IPrKzwxLfutnACbncMmx2Ce57xCesU4QU",
-};
-
-push.setVapidDetails(
-  "mailto:eric.hopper@cozera.io",
-  keys.publicKey,
-  keys.privateKey
-);
-
-// the following json object is created and logged to the console in the index.html subscribe() function
-// this value would normally be logged into a database. Used to send push notification back to the browser.
-let sub = {
   endpoint:
     "https://fcm.googleapis.com/fcm/send/fhT90wDOmY8:APA91bG5r-GxIS1tHhOdqP7DmSkaf9DGYCgaCZDreiL_pPfBI-5uUlK5ezgUdtm2rjfvj2on3hKd_s5P8ZKa-MlAsrICRlQFfMc1fuP0j5ABSMTd6iTnCxKoy1K0sfjBy45YPbt8pQ8G",
   expirationTime: null,
@@ -27,4 +13,4 @@ let sub = {
     auth: "09-8SL_Sw2MTRKfWsYqPOw",
   },
 };
-push.sendNotification(sub);
+push.sendNotification(keys);
