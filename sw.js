55eafa1b0dec7c51df00749f4d0400c67fc2a1a9
eric hopper <eric.hopper@cozera.io>
2022-06-18 08:04:28 -0700

    adding logic to set focus, or open window

--- sw.js
+++ sw.js
@@ -2,9 +2,34 @@ self.addEventListener('push', (e) => {
 
   //const data = e.data.json();
   
-  self.addEventListener('notificationclick', function(e) {
+  self.addEventListener('notificationclick', (e) => {
     //var doge = e.notification.data.doge;
     window.open('https://agent.dev.cozera.io/#demo1');
+  });
+
+  self.addEventListener('notificationclick', (event) => {
+    console.log('On notification click: ', event.notification?.tag);
+    // Android doesn't close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+      clients.matchAll({
+        type: "window"
+      })
+      .then((clientList) => {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == '/' && 'focus' in client)
+            return client.focus();
+        }
+        if (clients.openWindow) {
+          return clients.openWindow('/');
+        }
+      })
+    );
   });
 
   const title = 'id-go Authentication request';
